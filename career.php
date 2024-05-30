97ea62fccc52c641d05aef4bb1e980932f8e0f7c
bishnusharm1 <bishnu.sharma@plutusco.com>
2024-05-30 11:22:44 +0530

    seo update

--- career.php
+++ career.php
@@ -4,6 +4,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="Plutus Consulting, Careers, Job opportunities, Employment, Work with us, Career growth, Job openings, Professional development, Join our team, Work culture, Employee benefits, Talent retention">
+<meta name="description" content="Explore career opportunities at Plutus Consulting. Join our team of experts and grow your career with us. Discover job openings, professional development opportunities, and our supportive work culture. Apply now and be part of a dynamic team.">
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Favicons -->
